13f3eaca1617c5ec9d7c424b7c62d524747a4ca9
duaghwns <5-5-5-5@naver.com>
2021-10-06 17:53:08 +0900

    Initial commit

---
+++ src/main/java/hojoon/web_AWS_study/WebAwsStudyApplication.java
@@ -0,0 +1,13 @@
+package hojoon.web_AWS_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAwsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAwsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hojoon/web_AWS_study/WebAwsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hojoon.web_AWS_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAwsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
