19652f5ca232f9bfda3a0bc5bec7400c8551248d
duaghwns <hj2758@gmail.com>
2021-10-12 10:14:24 +0900

    PostsRepository 생성

---
+++ src/main/java/hojoon/web_AWS_study/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package hojoon.web_AWS_study.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
