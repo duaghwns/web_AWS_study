7f49695616a6aa1075b7843d1f6aedd67b9960f1
duaghwns <hj2758@gmail.com>
2021-10-13 17:20:17 +0900

    JPA Auditing 활성화

--- src/main/java/hojoon/web_AWS_study/WebAwsStudyApplication.java
+++ src/main/java/hojoon/web_AWS_study/WebAwsStudyApplication.java
@@ -2,7 +2,9 @@ package hojoon.web_AWS_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class WebAwsStudyApplication {
 	public static void main(String[] args) { SpringApplication.run(WebAwsStudyApplication.class, args); }
