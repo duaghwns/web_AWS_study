162158c4b9bdcb99606d5883a76b853506517879
duaghwns <hj2758@gmail.com>
2021-10-13 14:05:49 +0900

    update, findById 추가

--- src/main/java/hojoon/web_AWS_study/domain/service/posts/PostsService.java
+++ src/main/java/hojoon/web_AWS_study/domain/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package hojoon.web_AWS_study.domain.service.posts;
 
+import hojoon.web_AWS_study.domain.posts.Posts;
 import hojoon.web_AWS_study.domain.posts.PostsRepository;
+import hojoon.web_AWS_study.web.dto.PostsResponseDto;
 import hojoon.web_AWS_study.web.dto.PostsSaveRequestDto;
+import hojoon.web_AWS_study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +21,15 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id : "+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
 
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id : " + id));
+        return new PostsResponseDto(entity);
+    }
 }
