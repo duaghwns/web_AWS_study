a5910c0ba869f39fb0a4200c953cf725a2575844
duaghwns <hj2758@gmail.com>
2021-10-14 17:59:36 +0900

    수정 완료시 로케이션 설정

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -41,13 +41,14 @@ var main = {
         var id = $('#id').val();
 
         $.ajax({
-            type: 'PUT',
+            type: 'POST',
             url: '/api/v1/posts/' + id,
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
         });
