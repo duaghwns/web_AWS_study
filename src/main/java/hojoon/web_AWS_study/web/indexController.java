e929459ac3dfe8ed3c3ec6e1fc3d040234cd396f
duaghwns <hj2758@gmail.com>
2021-10-14 17:14:46 +0900

    RequiredArgsConstructor 추가

--- src/main/java/hojoon/web_AWS_study/web/indexController.java
+++ src/main/java/hojoon/web_AWS_study/web/indexController.java
@@ -1,16 +1,17 @@
 package hojoon.web_AWS_study.web;
 
 import hojoon.web_AWS_study.domain.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.service.spi.InjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@RequiredArgsConstructor
 @Controller
 public class indexController {
 
-    private PostsService postsService;
+    private final PostsService postsService;
 
     @GetMapping("/")
     public String index(Model model) {
