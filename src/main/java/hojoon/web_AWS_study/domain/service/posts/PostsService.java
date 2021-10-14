a71d2c6aeaeea16a604a8a928299255c240a5556
duaghwns <hj2758@gmail.com>
2021-10-14 16:40:56 +0900

    조회 메서드 추가

--- src/main/java/hojoon/web_AWS_study/domain/service/posts/PostsService.java
+++ src/main/java/hojoon/web_AWS_study/domain/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package hojoon.web_AWS_study.domain.service.posts;
 
 import hojoon.web_AWS_study.domain.posts.Posts;
 import hojoon.web_AWS_study.domain.posts.PostsRepository;
+import hojoon.web_AWS_study.web.dto.PostsListResponseDto;
 import hojoon.web_AWS_study.web.dto.PostsResponseDto;
 import hojoon.web_AWS_study.web.dto.PostsSaveRequestDto;
 import hojoon.web_AWS_study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id : " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
