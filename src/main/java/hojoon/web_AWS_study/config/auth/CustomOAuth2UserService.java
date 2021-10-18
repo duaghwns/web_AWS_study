90dfb7bec8c074988f3e236a15e04c860b1a4e47
duaghwns <hj2758@gmail.com>
2021-10-18 09:15:14 +0900

    CustomOAuth2UserService 코드 완성

--- src/main/java/hojoon/web_AWS_study/config/auth/CustomOAuth2UserService.java
+++ src/main/java/hojoon/web_AWS_study/config/auth/CustomOAuth2UserService.java
@@ -1,16 +1,20 @@
 package hojoon.web_AWS_study.config.auth;
 
+import hojoon.web_AWS_study.domain.User.User;
 import hojoon.web_AWS_study.domain.User.UserRepository;
 import hojoon.web_AWS_study.web.dto.OAuthAttributes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 @Service
@@ -32,6 +36,19 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                                                     .getUserInfoEndpoint()
                                                     .getUserNameAttributeName();
 
-        OAuthAttributes attributes = OAuthAttributes.of()
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+        HttpSession.setAttribute("user",new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey()))
+                                                                            , attributes.getAttributes()
+                                                                            , attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail()).map(entity -> entity.update(attributes.getName(), attributes.getPicture())).orElse(attributes.toEntity());
+
+        return userRepository.save(user);
     }
 }
